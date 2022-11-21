7eb664279949b3bc19935cd8ed7fad346d8f2a8d
aaronliuchao <aaronliuchao@didiglobal.com>
2022-11-21 09:10:02 +0800

    添加跳转截屏Activity

--- app/src/main/java/driver/chao/com/qtan/StartActivity.java
+++ app/src/main/java/driver/chao/com/qtan/StartActivity.java
@@ -3,10 +3,10 @@ package driver.chao.com.qtan;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import driver.chao.com.qtan.screen.ScreenActivity;
 import driver.chao.com.qtan.video.VideoActivity;
 
 public class StartActivity extends AppCompatActivity {
@@ -15,7 +15,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
+        // 跳转制作视频
         findViewById(R.id.start_goto_video).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,7 +23,7 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        // 跳转盘口验证
         findViewById(R.id.start_goto_pan).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,11 +31,11 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        // 跳转制作截屏
         findViewById(R.id.start_goto_screen).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(StartActivity.this, VideoActivity.class);
+                Intent intent = new Intent(StartActivity.this, ScreenActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/driver/chao/com/qtan/screen/ScreenActivity.java
@@ -0,0 +1,17 @@
+package driver.chao.com.qtan.screen;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import driver.chao.com.qtan.R;
+
+public class ScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+    }
+}
