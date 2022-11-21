aebf51e844b12155e6480849def7815579844dd1
aaronliuchao <aaronliuchao@didiglobal.com>
2022-11-21 10:46:01 +0800

    添加截屏功能

--- app/src/main/java/driver/chao/com/qtan/screen/ScreenActivity.java
+++ app/src/main/java/driver/chao/com/qtan/screen/ScreenActivity.java
@@ -1,6 +1,9 @@
 package driver.chao.com.qtan.screen;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +16,19 @@ public class ScreenActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen);
+
+        findViewById(R.id.screen_button_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = ((EditText) findViewById(R.id.screen_title_edt_content)).getText().toString();
+                String time = ((EditText) findViewById(R.id.screen_time_edt_content)).getText().toString();
+                String content = ((EditText) findViewById(R.id.screen_content_edt_content)).getText().toString();
+
+                ((TextView) findViewById(R.id.screen_name_tv)).setText(title);
+                ((TextView) findViewById(R.id.screen_time_tv)).setText(time);
+                ((TextView) findViewById(R.id.screen_time_tv2)).setText(time);
+                ((TextView) findViewById(R.id.screen_content_tv)).setText(content);
+            }
+        });
     }
 }
